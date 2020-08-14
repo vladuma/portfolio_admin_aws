38b1b2d40e415303d25761bfe56208d856c4c498
Vlad Duma <vlad.duma@deplabs.com>
2020-08-14 11:56:00 +0300

    basic api setup

--- handlers/delete.js
+++ handlers/delete.js
@@ -1,18 +1,17 @@
 'use strict';
 
 module.exports.del = async event => {
+  const {dbName} = JSON.stringify(event || event.body);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Deleted successfully: ${dbName}`,
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
--- handlers/update.js
+++ handlers/update.js
@@ -1,18 +1,17 @@
 'use strict';
 
 module.exports.put = async event => {
+  const {dbName, data} = JSON.stringify(event || event.body);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Updated successfully: ${dbName}`,
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
